83aa48d1df2a0f9eb024e9e5a2492d252ed6dee3
Rahul <rahul.cocoalabs.com>
2020-08-13 12:21:53 +0530

    1st push

---
+++ config.js
@@ -0,0 +1,52 @@
+module.exports = {
+	port:3030,
+	routes:  [
+		{
+			path:"accounts",
+			port:"3031"
+		},
+		// {
+		// 	path:"pasters",
+		// 	port:"3002"
+		// },
+		// {
+		// 	path:"events",
+		// 	port:"3003"
+		// },
+		// {
+		// 	path:"groups",
+		// 	port:"3004"
+		// },
+		// {
+		// 	path:"masters",
+		// 	port:"3005"
+		// },
+		// {
+		// 	path:"buyorsell",
+		// 	port:"3006"
+		// },
+		// {
+		// 	path:"feeds",
+		// 	port:"3007"
+		// },
+		// {
+		// 	path:"matrimonies",
+		// 	port:"3008"
+		// },
+		// {
+		// 	path:"bloodDonation",
+		// 	port:"3009"
+		// },
+		// {
+		// 	path:"charities",
+		// 	port:"3010"
+		// },
+		// {
+		// 	path:"urogulf",
+		// 	port:"3011"
+		// }
+
+
+	]
+
+}
