cc9871eb70699e61935f025b51de4b17fa1c4e2f
Rahul <rahul.cocoalabs.com>
2020-08-18 21:48:19 +0530

    change

--- config.js
+++ config.js
@@ -9,42 +9,42 @@ module.exports = {
 			path:"users",
 			port:"3032"
 		},
-		// {
-		// 	path:"events",
-		// 	port:"3033"
-		// },
-		// {
-		// 	path:"groups",
-		// 	port:"3034"
-		// },
+		{
+			path:"events",
+			port:"3033"
+		},
+		{
+			path:"groups",
+			port:"3034"
+		},
 		{
 			path:"masters",
 			port:"3035"
 		},
-		// {
-		// 	path:"buyorsell",
-		// 	port:"3036"
-		// },
+		{
+			path:"buyorsell",
+			port:"3036"
+		},
 		{
 			path:"feeds",
 			port:"3037"
 		},
-		// {
-		// 	path:"matrimonies",
-		// 	port:"3038"
-		// },
-		// {
-		// 	path:"bloodDonation",
-		// 	port:"3039"
-		// },
-		// {
-		// 	path:"charities",
-		// 	port:"3040"
-		// },
-		// {
-		// 	path:"urogulf",
-		// 	port:"3041"
-		// }
+		{
+			path:"matrimonies",
+			port:"3038"
+		},
+		{
+			path:"bloodDonation",
+			port:"3039"
+		},
+		{
+			path:"charities",
+			port:"3040"
+		},
+		{
+			path:"urogulf",
+			port:"3041"
+		}
 
 
 	]
